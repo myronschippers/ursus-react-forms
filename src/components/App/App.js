c27d7a5e9dcef99e3b3c00ef52e537d983e815d8
Myron <dubtwee@gmail.com>
2020-04-08 09:15:26 -0500

    removing code that wasn't needed for lecture

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,43 +3,6 @@ import Header from '../Header/Header';
 import Counter from '../Counter/Counter';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      text: 'Hello Ursus',
-      headingText: 'My Heading',
-      pageHeading: 'React Local State',
-    };
-
-  }
-
-  changeWords = (event) => {
-    console.log('CHANGE WORDS!!!');
-    // the element is represented by...
-    const elementChanged = event.target;
-
-    this.setState({
-      text: elementChanged.value,
-    });
-  }
-
-  alterHeading() {
-    this.setState({
-      headingText: 'TOTES CHANGED IT'
-    });
-  }
-
-  clickMe = (event) => {
-    this.alterHeading();
-  }
-
-  clickGreeting = () => {
-    this.setState({
-      pageHeading: 'Hey There...'
-    });
-  }
-
   render() {
     return (
       <div>
